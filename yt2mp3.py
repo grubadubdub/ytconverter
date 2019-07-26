0c2f9c2765aca545c18a39797d6808499bc97a55
grubadubdub <gteptada@alumni.ubc.ca>
2019-07-26 13:26:42 -0700

    Add plan

--- yt2mp3.py
+++ yt2mp3.py
@@ -4,10 +4,11 @@ from bs4 import BeautifulSoup as leSoup
 # find source script scramble
 # get hash thing from scramble
 # send get to check.php
-# 	- callback: from network??
-# 	- f: unscramble
+# 	- callback:
+# 	- f: mp3
 # 	- v: video
-# 	- _: in scramble
+#   - k: unscramble
+# 	- _:
 
 data = requests.get('https://ytmp3.cc/')
 soup = leSoup(data.text, 'html.parser')
@@ -15,5 +16,16 @@ soup = leSoup(data.text, 'html.parser')
 scripts = soup.findAll('script')
 for script in scripts:
 	print(script['src'])
+
+data = {
+	'v':,
+	'f': 'mp3',
+	'k':,
+}
+
+
+
+def unscramble(h):
+	
 
 print(data.content)
